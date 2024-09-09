5955e0df424d47c30430b93403f0d05d96f72a58
Mr Jabes <jabessilva31@gmail.com>
2024-09-09 01:43:38 -0300

    Atualizações; Ajustes

--- src/main/java/com/springboot/primeiroprojetospringboot/model/User.java
+++ src/main/java/com/springboot/primeiroprojetospringboot/model/User.java
@@ -6,6 +6,14 @@ public class User {
     private String nome;
     private String email;
 
+    public User() {
+    }
+
+    public User(String nome, String email) {
+        this.nome = nome;
+        this.email = email;
+    }
+
     public String getNome() {
         return this.nome;
     }
