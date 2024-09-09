22d2a451454cd5000042636049c92340fb6bd721
Mr Jabes <jabessilva31@gmail.com>
2024-09-08 23:36:46 -0300

    Atualizações: Criação da Camada de Controller  e o Hello Controler

---
+++ src/main/java/com/springboot/primeiroprojetospringboot/controlers/HelloWordController.java
@@ -0,0 +1,8 @@
+package com.springboot.primeiroprojetospringboot.controlers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController// Suprime as anotações de Controller e Response Body
+public class HelloWordController {
+
+}
