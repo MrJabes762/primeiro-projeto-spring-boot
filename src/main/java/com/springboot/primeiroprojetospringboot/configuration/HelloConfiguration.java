65123879560668a73330baedae692cf26ba3f482
Mr Jabes <jabessilva31@gmail.com>
2024-09-09 01:26:08 -0300

    Atualizações: Criação do Hello World Confguration, Hello Services e Ajuste da Controller

---
+++ src/main/java/com/springboot/primeiroprojetospringboot/configuration/HelloConfiguration.java
@@ -0,0 +1,8 @@
+package com.springboot.primeiroprojetospringboot.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+}
--- src/main/java/com/springboot/primeiroprojetospringboot/controllers/HelloWorldController.java
+++ src/main/java/com/springboot/primeiroprojetospringboot/controllers/HelloWorldController.java
@@ -3,6 +3,8 @@ package com.springboot.primeiroprojetospringboot.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 
 @RestController
 @RequestMapping("/hello-world")// se a pagina fizer qualquer requisição nessa part ela mapeia pra o que estiver dentro dela 
@@ -12,4 +14,9 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
+    @PostMapping
+    public String helloWordPost() {
+        return "Hello Word Post";
+    }
+    
 }
