3acb78c212e57c6b5794d2dd6ad84f4371358ed4
Mr Jabes <jabessilva31@gmail.com>
2024-09-08 23:15:06 -0300

    first commit

---
+++ src/test/java/com/springboot/primeiroprojetospringboot/primeiro_projeto_spring_boot/PrimeiroProjetoSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.primeiroprojetospringboot.primeiro_projeto_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
