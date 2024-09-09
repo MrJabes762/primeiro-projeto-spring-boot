4b6bb05a99feb08b84a3b67222f5eac2ff9473a1
Mr Jabes <jabessilva31@gmail.com>
2024-09-09 01:51:51 -0300

    Atuaizações: Finalizando Projeto Basico

--- src/main/java/com/springboot/primeiroprojetospringboot/controllers/HelloWorldController.java
+++ src/main/java/com/springboot/primeiroprojetospringboot/controllers/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.springboot.primeiroprojetospringboot.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.primeiroprojetospringboot.model.User;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,9 +20,9 @@ public class HelloWorldController {
     public String helloWorld() {
         return "Hello World";
     }
-    @PostMapping ("") // Método Usado para a criação de novos recursos da API
-    public String helloWordPost(@RequestBody User body) {
-        return "Hello Word Post" + body.getNome();
+    @PostMapping ("/{id}") // Método Usado para a criação de novos recursos da API
+    public String helloWordPost(@PathVariable("id") String id, @RequestParam(defaultValue = "nenhun") String filter,@RequestBody User body) {
+        return "Hello Word Post" + body.getNome() + filter;
     }
     
 }
